19b53627c72c97acab4c3e5d2a85937b6dabff5e
zeroqs <smthq@bk.ru>
2023-06-18 21:04:28 +0300

    fix query post path

--- src/App/services/form.ts
+++ src/App/services/form.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const formApi = createApi({
   reducerPath: 'formApi',
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_ENDPOINT }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.sbercloud.ru/content/v1/bootcamp' }),
   endpoints: (builder) => ({
     postData: builder.mutation<FormState, FormState>({
       query: (body) => ({
